37bcc5e96bc0ff6b97f959175d81cec5a0919eaa
Neiro <gpbriefcase@gmail.com>
2021-05-17 12:48:50 -0500

    My first commit.

---
+++ global/src/test/java/edu/test/global/GlobalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.test.global;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
